385b2ba1b9bdcc218484a270d769e26220d30291
codeninjascollierville <107887805+codeninjascollierville@users.noreply.github.com>
2023-09-06 18:26:05 -0500

    step 8d

---
+++ Assets/Scripts/HUD.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUD : MonoBehaviour
+{
+    public Level level;
+    public Text remainingText;
+    public Text remainingSubtext;
+    public Text targetText;
+    public Text targetSubtext;
+    public Text scoreText;
+    public Image[] stars;
+
+    private int starIndex;
+    private bool isGameOver;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
