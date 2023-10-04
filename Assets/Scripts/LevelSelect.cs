bf62031d94648e50cdbde5af5cdbaea78416f7bd
codeninjascollierville <107887805+codeninjascollierville@users.noreply.github.com>
2023-10-04 18:14:13 -0500

    hi

--- Assets/Scripts/LevelSelect.cs
+++ Assets/Scripts/LevelSelect.cs
@@ -5,6 +5,25 @@ using UnityEngine.SceneManagement;
 
 public class LevelSelect : MonoBehaviour
 {
+    public void Start() {
+        for (int i = 0; i < buttons.Length; i++)
+        {
+            int score = PlayerPrefs.GetInt(buttons[i].playerPrefKey, 0);
+
+            for (int starIndex = 1; starIndex <= 3; starIndex++) 
+            {
+                Transform star = buttons[i].gameObject.transform.Find("star" + starIndex);
+                if (starIndex <= score) {
+                    star.gameObject.SetActive(true);
+                } 
+                else
+                {
+                    star.gameObject.SetActive(false);
+                }
+            }
+        }
+    }
+
     public void OnButtonPress(string levelName) {
         SceneManager.LoadScene(levelName);
     }
